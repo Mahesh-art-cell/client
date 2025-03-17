5117c696d620e124a9b6731e7830732916de9743
Mahesh-art-cell <tallurim886@email.com>
2025-03-17 15:37:13 +0530

    new code

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://server-wi41.onrender.com/api",
+  baseURL: "https://server-wi41.onrender.com",
   withCredentials: true,  // ✅ Ensures cookies are sent
 });
 
