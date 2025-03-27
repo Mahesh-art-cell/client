e2353fe0c0b2746993dabc5c66401883ea374c2f
Mahesh-art-cell <tallurim886@email.com>
2025-03-27 17:38:34 +0530

    new code

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,14 +1,14 @@
 import "./leftBar.scss";
 import Friends from "../../assets/1.png";
-import Groups from "../../assets/2.png";
+// import Groups from "../../assets/2.png";
 // import Market from "../../assets/3.png";
-import Watch from "../../assets/4.png";
+// import Watch from "../../assets/4.png";
 // import Memories from "../../assets/5.png";
 // import Events from "../../assets/6.png";
 // import Gaming from "../../assets/7.png";
-import Gallery from "../../assets/8.png";
-import Videos from "../../assets/9.png";
-import Messages from "../../assets/10.png";
+// import Gallery from "../../assets/8.png";
+// import Videos from "../../assets/9.png";
+// import Messages from "../../assets/10.png";
 // import Tutorials from "../../assets/11.png";
 // import Courses from "../../assets/12.png";
 // import Fund from "../../assets/13.png";
@@ -34,18 +34,18 @@ const LeftBar = () => {
             <img src={Friends} alt="" />
             <span>Friends</span>
           </div>
-          <div className="item">
+          {/* <div className="item">
             <img src={Groups} alt="" />
             <span>Groups</span>
-          </div>
+          </div> */}
           {/* <div className="item">
             <img src={Market} alt="" />
             <span>Marketplace</span>
           </div> */}
-          <div className="item">
+          {/* <div className="item">
             <img src={Watch} alt="" />
             <span>Watch</span>
-          </div>
+          </div> */}
           {/* <div className="item">
             <img src={Memories} alt="" />
             <span>Memories</span>
@@ -62,18 +62,18 @@ const LeftBar = () => {
             <img src={Gaming} alt="" />
             <span>Gaming</span>
           </div> */}
-          <div className="item">
+          {/* <div className="item">
             <img src={Gallery} alt="" />
             <span>Gallery</span>
-          </div>
-          <div className="item">
+          </div> */}
+          {/* <div className="item">
             <img src={Videos} alt="" />
             <span>Videos</span>
-          </div>
-          <div className="item">
+          </div> */}
+          {/* <div className="item">
             <img src={Messages} alt="" />
             <span>Messages</span>
-          </div>
+          </div> */}
         </div>
         <hr />
         {/* <div className="menu">
@@ -90,7 +90,7 @@ const LeftBar = () => {
             <img src={Courses} alt="" />
             <span>Courses</span>
           </div> */}
-        </div>
+        {/* </div> */}
       </div>
     // </div>
   );
