5acf2e96bc5066efa3ddc2114e61a90412b0587c
Mahesh-art-cell <tallurim886@email.com>
2025-03-28 11:32:30 +0530

    profile code

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,98 +1,78 @@
+// import "./leftBar.scss";
+// import Friends from "../../assets/1.png";
+// import { AuthContext } from "../../context/authContext";
+// import { useContext } from "react";
+
+// const LeftBar = () => {
+
+//   const { currentUser } = useContext(AuthContext);
+
+//   return (
+//     <div className="leftBar">
+//       <div className="container">
+//         <div className="menu">
+//           <div className="user">
+//             <img
+//               src={"/upload/" +currentUser.profilePic}
+//               alt=""
+//             />
+//             <span>{currentUser.name}</span>
+//           </div>
+//           <div>
+//             <span>Profile</span>
+
+//           </div>
+//           <div className="item">
+//             <img src={Friends} alt="" />
+//             <span>Friends</span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LeftBar;
+
+
 import "./leftBar.scss";
 import Friends from "../../assets/1.png";
-// import Groups from "../../assets/2.png";
-// import Market from "../../assets/3.png";
-// import Watch from "../../assets/4.png";
-// import Memories from "../../assets/5.png";
-// import Events from "../../assets/6.png";
-// import Gaming from "../../assets/7.png";
-// import Gallery from "../../assets/8.png";
-// import Videos from "../../assets/9.png";
-// import Messages from "../../assets/10.png";
-// import Tutorials from "../../assets/11.png";
-// import Courses from "../../assets/12.png";
-// import Fund from "../../assets/13.png";
 import { AuthContext } from "../../context/authContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Profile from "../../pages/profile/Profile"; // Import Profile Page
 
 const LeftBar = () => {
-
   const { currentUser } = useContext(AuthContext);
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleProfileClick = () => {
+    setShowProfile((prev) => !prev); // Toggle Profile Visibility
+  };
 
   return (
     <div className="leftBar">
       <div className="container">
         <div className="menu">
-          <div className="user">
+          {/* ✅ User Profile Section */}
+          <div className="user" onClick={handleProfileClick} style={{ cursor: "pointer" }}>
             <img
-              src={"/upload/" +currentUser.profilePic}
-              alt=""
+              src={currentUser.profilePic || "/default-avatar.png"}
+              alt="User Profile"
             />
             <span>{currentUser.name}</span>
           </div>
+
+          {/* ✅ Profile Menu */}
           <div className="item">
-            <img src={Friends} alt="" />
+            <img src={Friends} alt="Friends" />
             <span>Friends</span>
           </div>
-          {/* <div className="item">
-            <img src={Groups} alt="" />
-            <span>Groups</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Market} alt="" />
-            <span>Marketplace</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Watch} alt="" />
-            <span>Watch</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Memories} alt="" />
-            <span>Memories</span>
-          </div> */}
         </div>
-        <hr />
-        <div className="menu">
-          <span>Your shortcuts</span>
-          {/* <div className="item">
-            <img src={Events} alt="" />
-            <span>Events</span>
-          </div>
-          <div className="item">
-            <img src={Gaming} alt="" />
-            <span>Gaming</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Gallery} alt="" />
-            <span>Gallery</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Videos} alt="" />
-            <span>Videos</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Messages} alt="" />
-            <span>Messages</span>
-          </div> */}
-        </div>
-        <hr />
-        {/* <div className="menu">
-          <span>Others</span>
-          <div className="item">
-            <img src={Fund} alt="" />
-            <span>Fundraiser</span>
-          </div>
-          <div className="item">
-            <img src={Tutorials} alt="" />
-            <span>Tutorials</span>
-          </div>
-          <div className="item">
-            <img src={Courses} alt="" />
-            <span>Courses</span>
-          </div> */}
-        {/* </div> */}
       </div>
-    // </div>
+
+      {/* ✅ Show Profile Page when clicked */}
+      {showProfile && <Profile />}
+    </div>
   );
 };
 
