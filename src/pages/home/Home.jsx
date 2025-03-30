bf7d49a38b88ead46639f77849be43648357f99a
Mahesh-art-cell <tallurim886@email.com>
2025-03-30 18:16:58 +0530

    rectify

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,36 +1,17 @@
-// import Stories from "../../components/stories/Stories"
-// import Posts from "../../components/posts/Posts"
-// import Share from "../../components/share/Share"
-// import "./home.css"
-
-// const Home = () => {
-//   return (
-//     <div className="home">
-//       <Stories/>
-//       <Share/>
-//       <Posts/>
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
-import Stories from "../../components/stories/Stories";
-import Posts from "../../components/posts/Posts";
-import Share from "../../components/share/Share";
-import "./home.css";
+import Stories from "../../components/stories/Stories"
+import Posts from "../../components/posts/Posts"
+import Share from "../../components/share/Share"
+import "./home.css"
 
 const Home = () => {
   return (
     <div className="home">
-      <Stories />
-      <Share />
-      {/* ✅ Show All Posts for All Users */}
-      <Posts userId={null} />
+      <Stories/>
+      <Share/>
+      <Posts/>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
+
