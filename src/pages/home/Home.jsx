a608aab233e3f4686bb3ec415dcf8add47ef573e
Mahesh-art-cell <tallurim886@email.com>
2025-04-06 18:41:20 +0530

    change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,41 @@
-import Stories from "../../components/stories/Stories"
-import Posts from "../../components/posts/Posts"
-import Share from "../../components/share/Share"
-import "./home.css"
-import LeftBar from "../../components/leftBar/LeftBar"
+// import Stories from "../../components/stories/Stories"
+// import Posts from "../../components/posts/Posts"
+// import Share from "../../components/share/Share"
+// import "./home.css"
+// import LeftBar from "../../components/leftBar/LeftBar"
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <Stories/>
+//       <Share/>
+//       <Posts/>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+
+import Stories from "../../components/stories/Stories";
+import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
+import LeftBar from "../../components/leftBar/LeftBar";
+import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <LeftBar/>
-      <Stories/>
-      <Share/>
-      <Posts/>
+      <div className="homeContainer">
+        {/* <LeftBar /> */}
+        <div className="mainContent">
+          <Stories />
+          <Share />
+          <Posts />
+        </div>
+      </div>  
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
